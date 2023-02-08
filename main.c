cb1f2c6685a7fbd575c808ef84fed79f84934ff9
levkush <lev.kushtym@gmail.com>
2023-02-08 13:01:38 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+#define DEST_SIZE 40
+
+char sendToPowershell(char directory_name)
+{
+    system("powershell.exe -Command pwd");
+}
+
+char buildCommand(char args[], char path[])
+{
+    char command[1000] = "powershell.exe -Command Remove-Item ";
+
+    size_t length = strlen(args);
+
+    for (size_t iter = 0; iter < length; iter++)
+    {
+        if (args[iter] == 'f') {
+            strcat(command, "-Force ");
+        }
+
+        else if (args[iter] == 'c') {
+            strcat(command, "-Confirm ");
+        }
+        
+        else if (args[iter] == 'r') {
+            strcat(command, "-Recurse ");
+        }
+    }
+
+    strcat(command, path);
+    
+    system(command);
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    if( argc == 3 ) {
+        if (argv[1][0] == '-')
+        {
+            //printf("Sending command\n");
+            buildCommand(argv[1], argv[2]);
+        }
+        else {
+            printf("Invalid arguments.\n");
+        }
+    }
+    else if( argc > 3 ) {
+        printf("Too many arguments supplied.\n");
+    }
+    else {
+        printf("Two arguments expected.\n");
+    }
+    return 0;
+}
